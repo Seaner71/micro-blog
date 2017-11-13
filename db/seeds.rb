e2e3e47b712ef4f704c2a9590c930b28789787a3
Nicole W <nicolemairead4@gmail.com>
2017-11-13 17:46:25 -0500

    seed some people

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,8 @@
 # User.create(f_name: 'Bobby', l_name: 'McBobberson')
 User.destroy_all
 
-user = User.create(username: 'testing')
-user = User.create(username: 'nwitte')
-user = User.create(username: 'nmw44')
+user = User.create(username: 'nwitte', f_name: 'Nikki', l_name: 'Witte', email: 'nwitte@gmail.com')
+user = User.create(username: 'nmp4', f_name: 'Natalie', l_name: "Portman", email: 'natport@gmail.com')
 user = User.create(username: 'sean')
 user = User.create(username: 'ss71')
 user = User.create(username: 'natalietate')
