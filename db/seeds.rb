9dc4f396cb3b7b1758b846dc0db95621c5bfed26
echo 'hello world <ssmyth1071@gmail.com>
2017-11-13 17:51:04 -0500

    seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,7 @@ User.destroy_all
 
 user = User.create(username: 'nwitte', f_name: 'Nikki', l_name: 'Witte', email: 'nwitte@gmail.com')
 user = User.create(username: 'nmp4', f_name: 'Natalie', l_name: "Portman", email: 'natport@gmail.com')
-user = User.create(username: 'sean')
-user = User.create(username: 'ss71')
+user = User.create(username: 'sean', f_name: 'Sean', l_name: "Smyth", email: 'ssmyth1071@gmail.com')
+user = User.create(username: 'pdiddy', f_name: 'Sean', l_name: "Combs", email: 'pdiddy@badboy.com')
 user = User.create(username: 'clooney', f_name: 'George', l_name: 'Clooney', email: 'georgec@gmail.com')
 user = User.create(username: 'natalie', f_name: 'Natalie', l_name: 'Russo', email: 'martin.nat@gmail.com')
