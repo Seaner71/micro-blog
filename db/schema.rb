fcde9d51a011483b85710396b31e38735860ff2a
natalietate <martin.nat@gmail.com>
2017-11-13 12:09:54 -0500

    change table to collect timestamp

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171113160009) do
+ActiveRecord::Schema.define(version: 20171113170236) do
 
   create_table "posts", force: :cascade do |t|
     t.string "content"
     t.integer "user_id"
-    t.datetime "create_time"
+    t.datetime "created_at"
   end
 
   create_table "users", force: :cascade do |t|
