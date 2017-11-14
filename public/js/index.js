7f24f76ac822abe4c1314f99db7ec3d103838f3e
echo 'hello world <ssmyth1071@gmail.com>
2017-11-14 09:43:37 -0500

    add remaining characters counter

--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,6 @@
-console.log('Hello world');
+var calcChars = function (element) {
+   var maxlength = element.getAttribute("maxlength");
+   var chars = element.value.length;
+   var remaining = maxlength - chars;
+ document.querySelector(".postcount").innerHTML = remaining;
+};
