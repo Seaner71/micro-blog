2f975d5eaf66527bda4291d08d9500e1fb035650
echo 'hello world <ssmyth1071@gmail.com>
2017-11-14 19:05:00 -0500

    correct class name for query selector

--- public/js/index.js
+++ public/js/index.js
@@ -2,7 +2,7 @@ var calcChars = function (element) {
    var maxlength = element.getAttribute("maxlength");
    var chars = element.value.length;
    var remaining = maxlength - chars;
- document.querySelector(".postcount").innerHTML = remaining;
+   document.querySelector(".postcount").innerHTML = remaining;
 };
 
 
